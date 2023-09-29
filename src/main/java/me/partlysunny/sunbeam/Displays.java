33779aa16e570144d08b8df3b11654da57f88c4f
Steven Zhu <65844236+steveyxz@users.noreply.github.com>
2023-09-29 21:47:57 +1000

    Some more display changes

--- src/main/java/me/partlysunny/sunbeam/Displays.java
+++ src/main/java/me/partlysunny/sunbeam/Displays.java
@@ -1,11 +1,29 @@
 package me.partlysunny.sunbeam;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Display;
+import org.bukkit.entity.Player;
 import org.bukkit.util.Transformation;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 
 public class Displays {
 
     public static Transformation copy(Transformation original) {
-        return new Transformation(original.getTranslation(), original.getLeftRotation(), original.getScale(), original.getRightRotation());
+        return new Transformation(new Vector3f(original.getTranslation()), new Quaternionf(original.getLeftRotation()), new Vector3f(original.getScale()), new Quaternionf(original.getRightRotation()));
+    }
+
+    public static void scale(Transformation transformation, float scale) {
+        transformation.getScale().mul(scale);
+    }
+
+    public static void scale(Transformation transformation, float x, float y, float z) {
+        transformation.getScale().mul(x, y, z);
+    }
+
+    public static <T extends Display> T summonDisplay(Location location, Class<T> displayClass) {
+        assert location.getWorld() != null;
+        return location.getWorld().spawn(location, displayClass);
     }
 
 
