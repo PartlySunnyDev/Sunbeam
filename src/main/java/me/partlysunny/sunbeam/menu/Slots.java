83b33e5995d5449b0eb4a2bfcb353e128a2a8195
Steven Zhu <65844236+steveyxz@users.noreply.github.com>
2023-09-29 20:42:12 +1000

    Slots class for helping with constants

---
+++ src/main/java/me/partlysunny/sunbeam/menu/Slots.java
@@ -0,0 +1,28 @@
+package me.partlysunny.sunbeam.menu;
+
+import org.bukkit.inventory.EquipmentSlot;
+
+public class Slots {
+
+    public static final int HOTBAR_START = 0;
+    public static final int HOTBAR_END = 8;
+    public static final int ARMOR_BOOTS = 36;
+    public static final int ARMOR_HELMET = 39;
+    public static final int OFFHAND = 40;
+
+    public static int getHotbarSlot(int index) {
+        return HOTBAR_START + index;
+    }
+
+    public static int getEquipmentSlot(EquipmentSlot slot) {
+        return switch (slot) {
+            case HAND -> 0;
+            case FEET -> ARMOR_BOOTS;
+            case LEGS -> ARMOR_BOOTS + 1;
+            case CHEST -> ARMOR_HELMET - 1;
+            case HEAD -> ARMOR_HELMET;
+            default -> -1;
+        };
+    }
+
+}
