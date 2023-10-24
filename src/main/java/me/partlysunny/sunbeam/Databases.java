0dfb60d26006e6104afa4c98fd5b5381589f35cb
Steven Zhu <65844236+steveyxz@users.noreply.github.com>
2023-10-25 10:39:21 +1100

    Get database source easily from a config section

--- src/main/java/me/partlysunny/sunbeam/Databases.java
+++ src/main/java/me/partlysunny/sunbeam/Databases.java
@@ -2,6 +2,8 @@ package me.partlysunny.sunbeam;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import me.partlysunny.sunbeam.config.Config;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import javax.sql.DataSource;
 
@@ -16,6 +18,23 @@ public class Databases {
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
         return config;
+    }
+
+    /**
+     * Get a DataSource from a config file
+     * The config section will be as follows:<br>
+     *     host: localhost<br>
+     *     port: 3306<br>
+     *     database: name<br>
+     *     username: root<br>
+     *     password: password<br>
+     * @param configFile The file, must be registered in {@link Config}
+     * @param section The section where the database info is
+     * @return The DataSource
+     */
+    public static DataSource getFromConfig(String configFile, String section) {
+        YamlConfiguration config = Config.getConfig(configFile);
+        return getDataSource(config.getString(section + ".host"), config.getInt(section + ".port"), config.getString(section + ".database"), config.getString(section + ".username"), config.getString(section + ".password"));
     }
 
     public static DataSource getDataSource(String host, int port, String database, String username, String password) {
