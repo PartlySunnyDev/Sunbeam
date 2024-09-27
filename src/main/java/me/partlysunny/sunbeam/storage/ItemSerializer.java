1376bde1e0dd8f887d975400f89dc0f6fe91de6e
Steven Zhu <65844236+steveyxz@users.noreply.github.com>
2024-09-27 16:08:30 +1000

    Fix ItemSerializer for complex items

--- src/main/java/me/partlysunny/sunbeam/storage/ItemSerializer.java
+++ src/main/java/me/partlysunny/sunbeam/storage/ItemSerializer.java
@@ -1,24 +1,51 @@
 package me.partlysunny.sunbeam.storage;
 
-import com.google.common.reflect.TypeToken;
-import com.google.gson.Gson;
+import me.partlysunny.sunbeam.misc.ConsoleLogger;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
 
-import java.lang.reflect.Type;
-import java.util.Map;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
 
-public final class ItemSerializer {
-
-    private static final Gson gson = new Gson();
+public class ItemSerializer {
 
     public static String fromItem(ItemStack itemStack) {
-        Map<String, Object> serialize = itemStack.serialize();
-        return gson.toJson(serialize);
+        //Serialize the item(turn it into byte stream)
+        ByteArrayOutputStream io = new ByteArrayOutputStream();
+        try {
+            BukkitObjectOutputStream os = new BukkitObjectOutputStream(io);
+            os.writeObject(itemStack);
+            os.flush();
+        } catch (IOException e) {
+            ConsoleLogger.error("Failed to serialize item: " + itemStack);
+            return null;
+        }
+
+        byte[] serializedObject = io.toByteArray();
+
+        //Encode the serialized object into to the base64 format
+        return new String(Base64.getEncoder().encode(serializedObject));
+
     }
 
-    public static ItemStack fromString(String json) {
-        Type type = new TypeToken<Map<String, Object>>(){}.getType();
-        return ItemStack.deserialize(gson.fromJson(json, type));
+    public static ItemStack fromString(String base64) {
+        byte[] serializedObject = Base64.getDecoder().decode(base64);
+
+        //Input stream to read the byte array
+        ByteArrayInputStream in = new ByteArrayInputStream(serializedObject);
+        //object input stream to serialize bytes into objects
+        BukkitObjectInputStream is;
+        //Use the object input stream to deserialize an object from the raw bytes
+        try {
+            is = new BukkitObjectInputStream(in);
+            return (ItemStack) is.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
+
